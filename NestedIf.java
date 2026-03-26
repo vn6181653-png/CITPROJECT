3c87827224cb7af08531586a4126e7fd6f2f0b98
vn6181653-png <vn6181653@gmail.com>
2026-03-26 12:16:53 +0530

    upload  NestedIf.java

--- NestedIf.java
+++ NestedIf.java
@@ -1,11 +1,15 @@
+import java.util.Scanner;
+
 public class NestedIf {
     public static void main(String[] args) {
-        int age=65;
+       Scanner sc= new Scanner(System.in);
+       System.out.println("Enter the age "); 
+       int age =sc.nextInt();
         if(age>=18){
         if(age>=65){
-            System.out.println("Eligible to vote and avail government fund");
+            System.out.println("Eligible to vote and avail government fund");//65git 
         } else
-        System.out.println("we are not eligible to vote and avail government");
+        System.out.println("we are eligible to vote and not eligible to avail government");//18
         }else
             System.out.println("We are not eligible to vote");
     }
