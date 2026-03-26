e9ce2c21fb3890d9a1397c19d26cd0a6d8bd2a32
vn6181653-png <vn6181653@gmail.com>
2026-03-26 10:22:20 +0530

    Updated constrol structures

---
+++ ControlStructures.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ControlStructures {
+
+    public static void main(String[] args) {
+        // Scanner sc=new Scanner(System.in);
+        // System.out.println("Enter the age");
+        // int age=sc.nextInt();
+        // if(age>=18)
+        // System.out.println("Elegible for voting");
+        //sc.close();
+        // if else largest number
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("ENter the first number ");
+//         int a= sc.nextInt();
+//         System.out.println("Enter the second number ");
+//         int b= sc.nextInt();
+//         if(a>b)
+//             System.err.println("first number is larger");
+//         else
+//               System.err.println("second number is larger");
+//     }
+// }
