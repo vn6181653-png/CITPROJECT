35e110e6ba8ee001b9f3e1d2238a306742156f19
vn6181653-png <vn6181653@gmail.com>
2026-03-26 14:17:04 +0530

    upload  JumpStatement .java

--- JumpStatement.java
+++ JumpStatement.java
@@ -2,7 +2,7 @@ public class JumpStatement {
     public static void main(String[] args) {
         for(int i=1;i<=10;i++){
             if(i==5)
-                break;
+                continue;
             System.out.println("Value is:" +i);
         }
     }
