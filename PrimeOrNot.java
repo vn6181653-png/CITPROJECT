4780e360d1c787bd9b0bc5ccc7c239f44b004fb0
vn6181653-png <vn6181653@gmail.com>
2026-03-26 15:57:49 +0530

    upload  PrimeOrNot .java

--- PrimeOrNot.java
+++ PrimeOrNot.java
@@ -1,15 +1,14 @@
 import java.util.Scanner;
-
-public class PrimeOr{
+public class PrimeOrNot {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number");
         int n = sc.nextInt();
         boolean flag=true;
-        if(n<=1){
+        if(n<=1)
             flag=false;
         else{
-            for(int i=2;i<=math.sqrt(n);i++){
+            for(int i=2;i<=Math.sqrt(n);i++){
             if(n%i==0){
                 flag=false;
                 break;
@@ -18,7 +17,11 @@ public class PrimeOr{
     }
     if(flag)
         System.out.println(n+"is a prime Prime number ");
-        else System.out.println(n+"is not a prime number");
+        else 
+            System.out.println(n+"is not a prime number");
+            sc.close();
+   }
+}   
+
     
-    }    sc.close();
-}
+
