36bdf70e0ea8d27e2d0d6c4788b72d7bd9334d8a
vn6181653-png <vn6181653@gmail.com>
2026-03-27 14:38:47 +0530

    upload NestedLoop .java

---
+++ NestedLoop.java
@@ -0,0 +1,14 @@
+public class NestedLoop {
+    public static void main(String[] args) {
+        for(int i=1;i<=2;i++){
+            System.out.println("Tiger:");
+            for(int j=1;j<=2;j++){
+                System.out.println("   Lion");
+            }
+            for(int k=1;k<=3;k++) {
+                System.out.println("cat");
+            }     
+        }     
+    }
+    
+}
