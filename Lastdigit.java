99f45dbc8f1e19d5607d121668cd65acaea1f33c
vn6181653-png <vn6181653@gmail.com>
2026-03-27 11:29:42 +0530

    upload Lastdigit .java

---
+++ Lastdigit.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Lastdigit {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int num =sc.nextInt();
+        
+        int lastdigit =num%10;
+        System.out.println("Last digit is:" + lastdigit);
+    }    
+}
