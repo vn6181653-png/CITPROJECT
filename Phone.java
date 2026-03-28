fcb950d1ea19179638943dff872d474591bd2d7f
vn6181653-png <vn6181653@gmail.com>
2026-03-28 10:44:24 +0530

    upload  Phone .java

---
+++ Phone.java
@@ -0,0 +1,12 @@
+public class Phone {
+    String brand="iphone";
+    String model="16 pro max";
+    void call(){
+        System.out.println("calling from iphone");
+    }
+public static void main(String[] args) {
+    Phone p1= new Phone();
+    p1.call();
+}
+    
+}
