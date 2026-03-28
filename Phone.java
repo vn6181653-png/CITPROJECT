8e93950dc0425db222178430fea3f07519f056a0
vn6181653-png <vn6181653@gmail.com>
2026-03-28 10:50:44 +0530

    upload meesage .java

--- Phone.java
+++ Phone.java
@@ -2,11 +2,24 @@ public class Phone {
     String brand="iphone";
     String model="16 pro max";
     void call(){
-        System.out.println("calling from iphone");
+        System.out.println("Calling  from iphone");
+    }
+    void message(){
+        System.out.println("Message from iphone");
     }
 public static void main(String[] args) {
     Phone p1= new Phone();
+    Phone p2=new Phone();
+    Phone p3=new Phone();
+    Phone p4=new Phone();
     p1.call();
+    p2.call();
+    p3.call();
+    p4.call();
+    p1.message();
+    p2.message();
+    p3.message();
+    p4.message();
 }
     
 }
